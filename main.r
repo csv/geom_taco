8f117b89f3a7840a5ac8dde62a43f1f1cabea15d
Thomas Levine <_@thomaslevine.com>
2014-02-05 20:55:19 +0000

    adapt stuff

--- main.r
+++ main.r
@@ -10,30 +10,18 @@ GeomTaco <- proto(Geom, {
   draw_groups <- function(., ...) .$draw(...)
   draw <- function(., data, scales, coordinates, na.rm = FALSE, ...) {    
     data <- remove_missing(data, na.rm, 
-      c("x", "label","meat","salsa","lime","radish"), name = "geom_taco")
+      c("label","fill","salsa","lime","radish","guacamole","cilantro"), name = "geom_taco")
     if (empty(data)) return(zeroGrob())
-    
-    with(coord_transform(coordinates, data, scales), 
-      ggname(.$my_name(), pointsGrob(x, y, size=unit(size, "mm"), pch=shape, 
-      gp=gpar(col=alpha(colour, alpha), fill = alpha(fill, alpha), fontsize = size * .pt)))
-    )
+    print(data)   
   }
 
   draw_legend <- function(., data, ...) {
     data <- aesdefaults(data, .$default_aes(), list(...))
-    
-    with(data,
-      pointsGrob(0.5, 0.5, size=unit(size, "mm"), pch=shape, 
-      gp=gpar(
-        col=alpha(colour, alpha), 
-        fill=alpha(fill, alpha), 
-        fontsize = size * .pt)
-      )
-    )
+    print(data)   
   }
 
   default_stat <- function(.) StatIdentity
-  required_aes <- c("x", "y")
-  default_aes <- function(.) aes(shape=16, colour="black", size=2, fill = NA, alpha = NA)
-  
+  required_aes <- c("label")
+  default_aes <- function(.) aes(fill = 'carne asada',
+    salsa = FALSE, lime = FALSE, radish = FALSE, guacamole = FALSE, cilantro = FALSE)
 })
